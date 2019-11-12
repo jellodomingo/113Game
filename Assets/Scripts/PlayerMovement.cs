d9041045b7203fcdcac5e53936258b1129c8a7b0
Angelo <jellodomingo@gmail.com>
2019-11-12 04:35:58 -0800

    First Commit with Player Movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] int speed = 3;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Inputs
+         * 
+         * W - Up
+         * S - Down
+         * A - Left
+         * D - Right
+         * 
+         * */
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+}
